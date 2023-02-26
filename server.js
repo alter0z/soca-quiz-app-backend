0104ff6a0c8fd3cda955c638c0168f22379d2add
alter0z <afilaansori2602@gmail.com>
2023-02-26 19:08:20 +0700

    fix something

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to bezkoder application.' });
+  res.json({ message: 'Welcome to the jungle.' });
 });
 
 const db = require('./app/models');
